0572f6fa069611790671d02bb1b7b7528a4b46e8
Bert <bert@ev6.net>
2012-01-19 22:30:47 +0000

    Added stub validation functions

---
+++ validate.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+/* DNS record validation functions go here */
+
+function validate_a() {
+return TRUE;
+}
+
+function validate_aaaa() {
+return TRUE;
+}
+
+function validate_ptr() {
+return TRUE;
+}
+
+?>
